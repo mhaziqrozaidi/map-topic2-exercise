061c528dfdca52c12f8f6df470f0496049d3ee10
Haziq Rozaidi <193243546+mhaziqrozaidi@users.noreply.github.com>
2025-04-10 16:38:57 +0800

    Add main.dart

---
+++ dart_application/bin/main.dart
@@ -0,0 +1,8 @@
+// Course: SECJ3623 (Mobile Application Programming)
+// Section: 02
+// Author: Muhammad Haziq Bin Rozaidi
+// Date: April 10, 2025
+// Description: Topic 2 Exercise Sheet
+
+void main() {
+}
